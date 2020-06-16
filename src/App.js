361d3753269bcb7e7c3151a483e26153ef925e90
Maxx <maksim.skomorokh@gmail.com>
2020-06-16 16:20:26 +0300

    delete item from todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -19,12 +20,23 @@ function App() {
     )
   }
 
+  function removeTodo(id) {
+    setTodos(
+      todos.filter(todo => todo.id !== id)
+    )
+  }
+
   return (
-    <div className="wrapper">
-      <h1>React Tutorial</h1>
-      <TodoList todos={todos} onToggle={toggleTodo}/>
-    </div>
-  );
+    <Context.Provider value={{removeTodo}}>
+      <div className="wrapper">
+        <h1>React Tutorial</h1>
+        {todos.length
+          ? <TodoList todos={todos} onToggle={toggleTodo}/>
+          : <p>No Todos</p>}
+
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App;
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from 'prop-types'
+import Context from "../context";
 
 const styles = {
   li: {
@@ -17,6 +18,8 @@ const styles = {
 }
 
 function TodoItem({todo, index, onChange}) {
+  const {removeTodo} = useContext(Context)
+
   let classes = []
   if (todo.completed) {
     classes.push('done')
@@ -33,7 +36,9 @@ function TodoItem({todo, index, onChange}) {
         &nbsp;
         {todo.title}
       </span>
-      <button className="rm">&times;</button>
+      <button className="rm"
+              onClick={() => removeTodo(todo.id)}
+      >&times;</button>
     </li>
   )
 }
