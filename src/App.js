0143c4dcf702de44151e92796e875f4421a6b63b
Maxx <maksim.skomorokh@gmail.com>
2020-06-16 15:16:09 +0300

    change state of todos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import TodoList from "./Todo/TodoList";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'Buy a bread'},
     {id: 2, completed: false, title: 'Buy a milk'},
     {id: 3, completed: false, title: 'Buy a chocolate'}
-  ]
+  ])
+
+
+  function toggleTodo(id) {
+    setTodos(todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
   return (
     <div className="wrapper">
       <h1>React Tutorial</h1>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   );
 }
 
-  export default App;
+export default App;
