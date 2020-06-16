e18229fa74f92a999f70f6b22bf7247e69fed8af
Maxx <maksim.skomorokh@gmail.com>
2020-06-16 13:54:05 +0300

    todo components added and displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import TodoList from "./Todo/TodoList";
 
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Buy a bread'},
+    {id: 2, completed: false, title: 'Buy a milk'},
+    {id: 3, completed: false, title: 'Buy a chocolate'}
+  ]
   return (
-    <div/>
+    <div className="wrapper">
+      <h1>React Tutorial</h1>
+      <TodoList todos={todos}/>
+    </div>
   );
 }
 
-export default App;
+  export default App;
