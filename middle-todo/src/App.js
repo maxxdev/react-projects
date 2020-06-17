b7ad32a6e2c9e80bceda38a29fa8f6a7edf1d38b
Maxx <maksim.skomorokh@gmail.com>
2020-06-17 18:16:24 +0300

    added navigation

--- middle-todo/src/App.js
+++ middle-todo/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
+import {Navbar} from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <div className="container">
         <Switch>
           <Route path={'/'} exact component={Home}/>
--- middle-todo/src/pages/About.js
+++ middle-todo/src/pages/About.js
@@ -2,8 +2,12 @@ import React, {Fragment} from "react";
 
 export const About = () => {
   return (
-    <Fragment>
-      <h1>About page</h1>
-    </Fragment>
+    <div className="jumbotron">
+      <h1 className="display-4">Hello, world!</h1>
+      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+        to featured content or information.</p>
+      <hr className="my-4"/>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    </div>
   )
 }
