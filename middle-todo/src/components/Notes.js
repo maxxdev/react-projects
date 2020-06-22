5af0d7f64c39527a6fcd7c5f25b28163c4bd2c63
Maxx <maksim.skomorokh@gmail.com>
2020-06-22 13:44:30 +0300

    Added animation

--- middle-todo/src/components/Notes.js
+++ middle-todo/src/components/Notes.js
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import {AlertContext} from "../context/alert/alertContext";
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 export const Notes = ({notes, onRemove}) => {
   const alert = useContext(AlertContext)
@@ -9,20 +10,27 @@ export const Notes = ({notes, onRemove}) => {
     })
   }
   return (
-    <ul className="list-group">
+    <TransitionGroup component="ul" className="list-group">
       {notes.map(note => (
-        <li className="list-group-item note"
-            key={note.id}
+        <CSSTransition
+          key={note.id}
+          classNames={'note'}
+          timeout={
+            {enter: 1000, exit: 350}
+          }
         >
-          <div>
-            <strong>{note.title}</strong>
-            <small>{new Date().toLocaleDateString()}</small>
-          </div>
-          <button type="button"
-                  onClick={() => removeItem(note.id)}
-                  className="btn btn-outline-danger btn-sm">&times;</button>
-        </li>
+          <li className="list-group-item note">
+            <div>
+              <strong>{note.title}</strong>
+              <small>{new Date().toLocaleDateString()}</small>
+            </div>
+            <button type="button"
+                    onClick={() => removeItem(note.id)}
+                    className="btn btn-outline-danger btn-sm">&times;</button>
+          </li>
+        </CSSTransition>
       ))}
-    </ul>
+    </TransitionGroup>
+
   )
 }
